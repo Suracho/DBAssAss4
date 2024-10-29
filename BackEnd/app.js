1d4bd792ab57ab9c26272193083a9a0e027a8c6e
Suraj <suracho@outlook.com>
2024-10-29 17:29:12 +1100

    init front and back

---
+++ BackEnd/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');  
+});
+
+app.post('/submit-form', (req, res) => {
+  res.send('Form submitted');
+}); 
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);  
+});
